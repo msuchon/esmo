d4e2dd3a327d7084178796233341fdcdc506d635
Michał Suchoń <michal.suchon@cyfroweszkoly.pl>
2023-10-03 18:14:17 +0200

    prac część kolejna

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ include('functions.php');
     <title>eSportowe Mistrzostwa Olsztyna</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <link href="https://vjs.zencdn.net/8.5.2/video-js.css" rel="stylesheet" />
 </head>
 <body ondragstart="return false;" ondrop="return false;">
 <div id="preloader">
@@ -53,6 +54,7 @@ include('functions.php');
         <a href="#" class="closeBtn" id="closebtn"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></a>
         <div id="nav_options" class="nav flex-column nav-underline">
         <img src="bitmapa.svg" alt="logo" style="max-width: 10vh; max-height: 10vh; margin: 0 auto;">
+        <a class="nav-link" href="#about">Start</a>
         <a class="nav-link" href="#about">O Turnieju</a>
         <a class="nav-link" data-bs-toggle="modal" data-bs-target="#registrationInfo">Zapisy</a>
         <a class="nav-link" href="#rules">Regulamin</a>
@@ -136,8 +138,58 @@ include('functions.php');
         </div>
     </section>
     <section>
-        <p>fedu</p>
+    <div class="media">
+    <h1>Zobacz Teaser</h1>
+  
+    <video
+    id="my-video"
+    class="video-js"
+    controls
+    preload="auto"
+    poster=""
+    data-setup="{}"
+    auto="true"
+    fluid="true"
+    style="box-shadow: 0px 0px 15px #1d3d72;"
+  >
+    <source src="content/trailer.mp4" type="video/mp4" frameborder="0"/>
+  </video>
+    </div>
     </section>
+    <section>
+        <div id="contact">
+        <h1>Bądźmy w kontakcie.</h1>
+        <p>odwiedź nasze social media</p>
+      <div class="cards">
+      <a href="https://instagram.com/esportowemistrzostwaolsztyna" target="_blank">
+      <div class="card text-center" id="insta_card" style="width: 10em;">
+      <div class="card-body">
+        <h5 class="card-title">INSTAGRAM</h5>
+        <svg xmlns="http://www.w3.org/2000/svg" height="5em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+      </div>
+      </div>  
+      </a>
+      <a href="https://www.facebook.com/esportowemistrzostwaolsztyna" target="_blank">
+      <div class="card text-center" id="fb_card" style="width: 10em;">
+      <div class="card-body">
+        <h5 class="card-title">FACEBOOK</h5>
+        <svg xmlns="http://www.w3.org/2000/svg" height="5em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"/></svg>
+      </div>
+      </div>
+      </a>
+
+      <a href="https://discord.gg/6XRJRh2x4J" target="_blank">
+      <div class="card text-center" id="dc_card" style="width: 10em;">
+      <div class="card-body">
+        <h5 class="card-title">DISCORD</h5>
+        <svg xmlns="http://www.w3.org/2000/svg" height="5em" viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"/></svg>
+      </div>
+      </div>
+      </a>
+      </div>
+      <a href="https://youtu.be/ezvfuqd0ZiM?si=IW48_Nqf7FCwQGay?autoplay=1&rel=0"><p style="bottom: -262px; position: relative; display: flex; justify-content: center; align-items: center; font-size: 1em !important; color: #041024; text-transform: uppercase;">polityka prywatności</p></a>
+      </div>
+  </section>
 
 
 <!-- MODALS (wyskakujące okienka) -->
@@ -149,7 +201,7 @@ include('functions.php');
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" style="text-align: center;">
-      <iframe width="1000px" height="500px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=3l6mzPSrwUWA3aEUFyyHdc2MM0aJJSNFkXDi40tF3pNUNkFPWFUyUEZaVzNYMVRaME40RkVLUkFQUC4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+      <iframe width="100%" height="500px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=3l6mzPSrwUWA3aEUFyyHdc2MM0aJJSNFkXDi40tF3pNUNkFPWFUyUEZaVzNYMVRaME40RkVLUkFQUC4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" id="zapisy" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" style="margin: 0 auto;" data-bs-dismiss="modal">Zamknij</button>
@@ -160,6 +212,7 @@ include('functions.php');
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script  src="js/scrollToTop.js"></script>
+<script src="https://vjs.zencdn.net/8.5.2/video.min.js"></script>
 
 <script>
 
