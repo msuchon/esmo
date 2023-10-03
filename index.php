9133ebfa53d74bd9da2a30d63731284d2440f3df
Michał Suchoń <michal.suchon@cyfroweszkoly.pl>
2023-10-03 19:42:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ include('functions.php');
         <a href="#" class="closeBtn" id="closebtn"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></a>
         <div id="nav_options" class="nav flex-column nav-underline">
         <img src="bitmapa.svg" alt="logo" style="max-width: 10vh; max-height: 10vh; margin: 0 auto;">
-        <a class="nav-link" href="index.php">Start</a>
+        <a class="nav-link" id="toTop">Start</a>
         <a class="nav-link" href="#about">O Turnieju</a>
         <a class="nav-link" data-bs-toggle="modal" data-bs-target="#registrationInfo">Zapisy</a>
         <a class="nav-link" href="#rules">Regulamin</a>
